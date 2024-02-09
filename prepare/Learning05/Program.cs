a1a803452708da622bd1ed41ac490e4b312e55fb
zachwetsel <zachwetsel@yahoo.com>
2024-02-08 19:23:25 -0700

    Finished Learning Activity 5

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        Square s1 = new Square("Green",12);
+        shapes.Add(s1);
+
+        Rectangle r1 = new Rectangle("Pink",4,6);
+        shapes.Add(r1);
+
+        Circle c1 = new Circle("Brown",9);
+        shapes.Add(c1);
+
+        foreach(Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+            Console.WriteLine($"{color} {area}");
+        }
     }
 }
